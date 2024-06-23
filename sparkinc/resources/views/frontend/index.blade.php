43f8319aea1eb3d04c011c42993c7ad8b6ac770d
iamsahilr <srajbhandari63@gmail.com>
2024-06-23 07:55:47 -0500

    homepage completed

--- sparkinc/resources/views/frontend/index.blade.php
+++ sparkinc/resources/views/frontend/index.blade.php
@@ -265,6 +265,46 @@
 
     </section>
 
+
+    <footer>
+        <div class="quicklinks">
+            <div class="company-short">
+                <p><img src="{{asset('images/spark.png')}}" alt=""></p>
+                <p>Revolutionizing health and vitality with cutting-edge technology and a commitment to personalized wellbeing.</p>
+            </div>
+            <div class="lists">
+                <p>About Us</p>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Blogs</a></li>
+                </ul>
+            </div>
+            <div class="lists">
+                <p>Quicklinks</p>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Blogs</a></li>
+                </ul>
+            </div>
+            <div class="lists">
+                <p>Privacy</p>
+                <ul>
+                    <li><a href="#">Privacy Policy</a></li>
+                    <li><a href="#">Terms and Conditions</a></li>
+                    <li><a href="#">FAQs</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="inner-footer">
+            <p>
+                Copyright <sup>&copy;</sup> Spark Inc Pvt Ltd | Nepal
+            </p>
+        </div>
+    </footer>
+
     
 
 
