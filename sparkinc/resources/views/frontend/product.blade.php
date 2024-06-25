632f4cdae1afea740f5091f6ed12032fe5869563
iamsahilr <srajbhandari63@gmail.com>
2024-06-25 12:17:52 -0500

    blog page set up

--- sparkinc/resources/views/frontend/product.blade.php
+++ sparkinc/resources/views/frontend/product.blade.php
@@ -10,7 +10,7 @@
     <article>Ensure you're always prepared for emergencies with our comprehensive medical kits, designed to provide essential first aid supplies for home, work, or travel.</article>
 
     <div class="products-container">
-        <div class="prod-cards" data-aos="fade-up" data-aos-duration="500">
+        <div class="prod-cards" data-aos="fade-up" data-aos-duration="800">
             <div class="prod-image">
                 <img src="{{asset('/images/about.jpg')}}" alt="product-image">
             </div>
@@ -38,7 +38,49 @@
             </div>
         </div>
 
-        <div class="prod-cards" data-aos="fade-up" data-aos-duration="1100">
+        <div class="prod-cards" data-aos="fade-up" data-aos-duration="800">
+            <div class="prod-image">
+                <img src="{{asset('/images/about.jpg')}}" alt="product-image">
+            </div>
+
+            <div class="prod-details">
+                <h1>Spandan ECG Kit</h1>
+                <p>Rs.500</p>
+                <p>
+                    <button class="btn btn-primary cart-btn">Add to Cart</button>
+                </p>
+            </div>
+        </div>
+
+        <div class="prod-cards" data-aos="fade-up" data-aos-duration="800">
+            <div class="prod-image">
+                <img src="{{asset('/images/about.jpg')}}" alt="product-image">
+            </div>
+
+            <div class="prod-details">
+                <h1>Spandan ECG Kit</h1>
+                <p>Rs.500</p>
+                <p>
+                    <button class="btn btn-primary cart-btn">Add to Cart</button>
+                </p>
+            </div>
+        </div>
+
+        <div class="prod-cards" data-aos="fade-up" data-aos-duration="800">
+            <div class="prod-image">
+                <img src="{{asset('/images/about.jpg')}}" alt="product-image">
+            </div>
+
+            <div class="prod-details">
+                <h1>Spandan ECG Kit</h1>
+                <p>Rs.500</p>
+                <p>
+                    <button class="btn btn-primary cart-btn">Add to Cart</button>
+                </p>
+            </div>
+        </div>
+
+        <div class="prod-cards" data-aos="fade-up" data-aos-duration="800">
             <div class="prod-image">
                 <img src="{{asset('/images/about.jpg')}}" alt="product-image">
             </div>
